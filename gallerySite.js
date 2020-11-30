af0b9acb4c3cf6ec49bfd50d57f6c3be5aa22917
Siksinats <67030519+Siksinats@users.noreply.github.com>
2020-11-30 09:58:22 -0500

    change gallery page to streamline process

--- gallerySite.js
+++ gallerySite.js
@@ -138,221 +138,75 @@ $(window).on('click', function(e) {
         $('.comic-blade').removeClass('bladeIn').addClass('bladeOut')
     }
 })
-
-
-
-
 /*--/Comic Script--*/
 
 
    
 /*--Commission Script--*/
 
-/********************************************/
-    /* Choose the style of the drawing*/
-/********************************************/
-$('.style-img').on('click', function(event) {
-    $(event.target).parent().addClass('selected')
-    $('.style-img').not('.selected').addClass('fast-shrink')
-
-
-    /*---------------------------------------
-        -Set style title to invisible
-        -Move style container to selection area
-        -Set not selected items to invisible
-    -----------------------------------------*/
-    setTimeout(function(){
-        $('.style-title').animate({
-            opacity: '0'
-        }, )
-
-        $('.style-container').animate({
-            left: '47px',
-            top: '40vh',
-            width: '200px'
-          }, 750);
-
-        $('.fast-shrink').animate({
-            width: '0px',
-            opacity: '0'
-        }, 500);
-    },0);
-
-
-    /*----------------------------------------
-        -Set not selected items to no display
-    -----------------------------------------*/
-    setTimeout(function() {
-        $('.fast-shrink').css('display', 'none')
-    }, 850)
-
-    
-    /*-----------------------------------------
-        -Set line items to visible
-        -Set style title to have no display
-        -Set height of line items
-    ------------------------------------------*/
-    setTimeout(function() {
-        $('.line-title').css('display', 'block')
-        $('.line-wrapper').css('display', 'flex')
-        $('.line-container').css('display', 'flex')
-        $('.style-title').css('display', 'none')
-        $('.selected-item-shadow').css('display', 'initial')
-        
-        $('.line-wrapper').animate({
-            height: '80%'
-        },'slow')
-
-        $('.line-container').animate({
-            height: '400px'
-        },'slow')
-    },1000)
+$('.style-label').each(function(index, value) {
+    $(value).on('click', function(e) {
+        $('.selected').each(function(index, value) {
+            $(value).next().prop('checked', false)
+            $(value).removeClass('selected')
+        })
+        $(e.target).addClass('selected')
+    })
 })
-/********************************************/
-    /* Choose the line work of the drawing*/
-/********************************************/
-$('.line-img').on('click', function(event) {
-    $(event.target).parent().addClass('selected')
-    $('.line-img').not('.selected').addClass('fast-shrink')
 
-    /*---------------------------------------
-        -Set line title to invisible
-        -Move line container to selection area
-        -Set not selected items to invisible
-    -----------------------------------------*/
-    setTimeout(function(){
-        $('.selected-item-shadow').animate({
-            opacity: '0'
+$('.chibi-color-label').each(function(index, value) {
+    $(value).on('click', function(e) {
+        $('.chibi-color-label').each(function(index, value) {
+
+            if ($(value).hasClass('selected')) {
+                $(value).removeClass('selected')
+            }
         })
+        $(e.target).addClass('selected')
+    })
+})
 
-        $('.line-title').animate({
-            opacity: '0'
-        }, )
+$('.size-label').each(function(index, value) {
+    $(value).on('click', function(e) {
+        $('.size-label').each(function(index, value) {
 
-        $('.style-container').animate({
-            opacity: '0'
+            if ($(value).hasClass('selected')) {
+                $(value).removeClass('selected')
+            }
+
+            $(value).siblings('div').children().each(function(index, value) {
+                if($(value)[0].checked == true) {
+                    $(value).prop('checked', false)
+                }
+
+                if($(value).hasClass('selected')) {
+                    $(value).removeClass('selected')
+                }
+                
+            })
+            
+        
         })
-
-        $('.line-container').animate({
-            left: '47px',
-            top: '40vh',
-            width: '200px'
-          }, 750);
-
-        $('.fast-shrink').animate({
-            width: '0px',
-            opacity: '0'
-        }, 500);
-    },0);
-
-    /*----------------------------------------
-        -Set not selected items to no display
-    -----------------------------------------*/
-    setTimeout(function() {
-        $('.fast-shrink').css('display', 'none')
-        $('.style-wrapper').css('display', 'none')
-        $('.selected-item-shadow').css('display','none')
-    }, 850)
-
-    
-    /*-----------------------------------------
-        -Set color items to visible
-        -Set line title to have no display
-        -Set height of color items
-    ------------------------------------------*/
-    setTimeout(function() {
-
-        $('.color-title').css('display', 'block')
-        $('.color-wrapper').css('display', 'initial')
-        $('.selected-item-shadow').css({
-            display: 'initial',
-            opacity: '100'})
-
         
-        $('.line-title').css({
-            height: '0px',
-            margin: '0px'
-        }, )
-        
-        $('.color-wrapper').animate({
-            height: '400px'
-        },'slow')
+        $(e.target).addClass('selected')
+    })
+})
 
-        $('.color-container').animate({
-            height: '400px'
-        },'slow')
-    },1000)
+$('.standard-color-label').each(function(index, value) {
+    $(value).on('click', function(e) {
+        $('.standard-color-label').each(function(index, value) {
+
+            if ($(value).hasClass('selected')) {
+                $(value).removeClass('selected')
+            }
+        })
+        $(e.target).addClass('selected')
+    })
 })
 
 
-/********************************************/
-    /* Choose the coloring of the drawing*/
-/********************************************/
-$('.color-img').on('click', function(event) {
-    $(event.target).parent().addClass('selected')
-    $('.color-img').not('.selected').addClass('fast-shrink')
-
-    /*---------------------------------------
-        -Set color title to invisible
-        -Move color container to selection area
-        -Set not selected items to invisible
-    -----------------------------------------*/
-    setTimeout(function(){
-        $('.selected-item-shadow').animate({
-            opacity: '0'
-        })
-        
-        $('.color-title').animate({
-            opacity: '0'
-        }, )
-
-        $('.line-container').animate({
-            opacity: '0'
-        })
-
-        $('.color-container').animate({
-            left: '47px',
-            top: '40vh',
-            width: '200px'
-          }, 750);
-
-        $('.fast-shrink').animate({
-            width: '0px',
-            opacity: '0'
-        }, 500);
-    },0);
-
-    /*----------------------------------------
-        -Set not selected items to no display
-    -----------------------------------------*/
-    setTimeout(function() {
-        $('.fast-shrink').css('display', 'none')
-        $('.line-wrapper').css('display', 'none')
-        $('.selected-item-shadow').css('display','none')
-    }, 850)
 
 
-
-    setTimeout(function() {
-        $('.details-title').css('display', 'block')
-       $('.form-wrapper').css('display', 'block')
-       $('.selected-item-shadow').css({
-        display: 'initial',
-        opacity: '100'})
-        
-        $('.color-title').css({
-            height: '0px',
-            margin: '0px'
-        }, )
-
-        $('.form-wrapper').animate({
-            height: '80vh'
-        },'slow')
-
-
-    },1000)
-
-})
         
  
 
